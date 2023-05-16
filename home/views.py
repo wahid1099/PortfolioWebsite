6b4a9623ed424b05e80d3229ec38d54b02da7b0b
wahid1099 <wahidahmed890@gmail.com>
2023-05-16 21:33:12 +0600

    adding blog projects

--- home/views.py
+++ home/views.py
@@ -1,24 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from projects.models import *
 from .models import *
 from blogs.models import *
-
+from django.core.mail import send_mail
+from django.contrib import messages
 # Create your views here.
 
 
 def home(request):
+   
+
 
     project=Project.objects.all()
     blog=Blog.objects.all()
-    social=SocailMedia.objects.all()
+    # social=SocailMedia.objects.all()
+    facebook_link = SocailMedia.objects.get(name='facebook')
+    linkedin_link = SocailMedia.objects.get(name='linkedin')
+    youtube_link = SocailMedia.objects.get(name='youtube')
+    github_link = SocailMedia.objects.get(name='github')
+    twitter_link = SocailMedia.objects.get(name='twitter')
+
     resume=ResumeLink.objects.first()
 
 
     context={
         'project':project,
         'blog':blog,
-        'social':social,
+        'facebook_link':facebook_link,
+        'linkedin_link':linkedin_link,
+        'youtube_link':youtube_link,
+        'github_link':github_link,
+        'twitter_link':twitter_link,
+
         'resume':resume
     }
+    if request.method=="POST":
+        message=request.POST["message"]
+        # print("message",message)
+        name=request.POST["name"]
+        email = request.POST["email"]
+        try:
+            send_mail(
+                'Contact Form',
+                f'From :{email}\n and\n Name : {name}\n{message}',
+                'mw0641295@gmail.com',
+                ['wahid55100@gmail.com'],
+                fail_silently=False,
+            ) 
 
-    return render(request,'index.html',context)
+            messages.success(request,"Message send successfully!!....")
+        except:
+            messages.error(request, "Email send Failed")
+        return render(request,'index.html',context)
+    else:
+
+     return render(request,'index.html',context)
+
+
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -140,3 +140,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS=True
+
